aa0adaef7467e709039542f6c4ede7cc65956c84
galen0 <ggong@umich.edu>
2013-10-27 18:04:00 -0400

    Added AdminPanel.cs files

---
+++ gui/AdminPanel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gui
+{
+    public partial class AdminPanel : Form
+    {
+        public AdminPanel()
+        {
+            InitializeComponent();
+        }
+
+        // Do something upon click on object list
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
