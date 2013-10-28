a935ed5c721dcf0ce7fd540d40950cf0c4d6bc2d
galen0 <ggong@umich.edu>
2013-10-28 12:25:49 -0400

    Added buttons to AdminPanel.Designer.cs to Get All Objects and Get Recent Objects

--- gui/AdminPanel.Designer.cs
+++ gui/AdminPanel.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.recentObjectsButton = new System.Windows.Forms.Button();
+            this.allObjectsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -46,15 +48,36 @@
             "9"});
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(565, 316);
+            this.listBox1.Size = new System.Drawing.Size(565, 290);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // recentObjectsButton
+            // 
+            this.recentObjectsButton.Location = new System.Drawing.Point(114, 308);
+            this.recentObjectsButton.Name = "recentObjectsButton";
+            this.recentObjectsButton.Size = new System.Drawing.Size(140, 58);
+            this.recentObjectsButton.TabIndex = 1;
+            this.recentObjectsButton.Text = "Get Recent Objects";
+            this.recentObjectsButton.UseVisualStyleBackColor = true;
+            // 
+            // allObjectsButton
+            // 
+            this.allObjectsButton.Location = new System.Drawing.Point(329, 308);
+            this.allObjectsButton.Name = "allObjectsButton";
+            this.allObjectsButton.Size = new System.Drawing.Size(150, 58);
+            this.allObjectsButton.TabIndex = 2;
+            this.allObjectsButton.Text = "Get All Objects";
+            this.allObjectsButton.UseVisualStyleBackColor = true;
+            this.allObjectsButton.Click += new System.EventHandler(this.button2_Click);
             // 
             // AdminPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(589, 378);
+            this.Controls.Add(this.allObjectsButton);
+            this.Controls.Add(this.recentObjectsButton);
             this.Controls.Add(this.listBox1);
             this.Name = "AdminPanel";
             this.Text = "Form2";
@@ -65,6 +88,8 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button recentObjectsButton;
+        private System.Windows.Forms.Button allObjectsButton;
 
 
     }
