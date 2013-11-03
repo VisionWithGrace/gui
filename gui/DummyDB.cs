658499e30665729ec4d02392803abc6d5d926f66
Thomas Lovett <tklovett@gmail.com>
2013-11-03 17:25:39 -0500

    create dummy db

---
+++ gui/DummyDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gui
+{
+    class Database
+    {
+        public string[] getSimilarTags(string partial)
+        {
+            return new string[] { "Hello", "Help", "Hell", "Hellow", "World", "These", "Are", "Similar" };
+        }
+    }
+}
